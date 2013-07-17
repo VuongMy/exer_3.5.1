15c460e63d5a72444ace10d5a8a3f6b27332148a
Vuong My <vuongmy91@gmail.com>
2013-07-17 09:13:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exer351::Application.config.secret_token = '61065a497fd8ffb76d9a20981484f3cf3741dc767353cf25bfc289fcbb30ac5db07be6b38522df0e91ac21832d6d373c8ab72ba1721fc80b9b540affbff369ec'
