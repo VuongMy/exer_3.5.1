aade4844bfdae1bb3e89c37281e602a5f4de24f2
Vuong My <vuongmy91@gmail.com>
2013-07-18 09:11:46 +0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,7 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
   end
   describe "Help page" do
   	it "should have the content 'Help'" do
